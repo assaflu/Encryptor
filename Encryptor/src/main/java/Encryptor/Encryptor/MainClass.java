98cb444b6b0f45a9c44e12847471ae8db2d8556f
assaflu <assaf.ss3@gmail.com>
2017-05-06 19:18:51 +0300

    file input was added - still needed to be checked

--- Encryptor/src/main/java/Encryptor/Encryptor/MainClass.java
+++ Encryptor/src/main/java/Encryptor/Encryptor/MainClass.java
@@ -1,5 +1,7 @@
 package Encryptor.Encryptor;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -12,13 +14,14 @@ public class MainClass
     public static void main( String[] args )
     {
     	Scanner reader = new Scanner (System.in);
+    	String userInput;
     	boolean flag = true;
     	System.out.println("Welcome to the Encryption and Decryption application");
     	while(flag){
             System.out.println("Choose your operation:");
             System.out.println("1.\tEncrypt file");
             System.out.println("2.\tDecrypt file");
-            String userInput = reader.nextLine();
+            userInput = reader.nextLine();
             if(userInput.length() != 1 ){
             	System.out.println("You should enter a number 1 for Encryption and 2 for Decryption\n");
             	continue;
@@ -38,6 +41,24 @@ public class MainClass
 			}
             
     	}
+    	flag = true;
+    	System.out.println("enter file path:");
+    	userInput = reader.nextLine();
+    	File inputFile = new File(userInput);
+    	while(flag){
+    		System.out.println("the path not either not leads to a file or not exists.\nenter new path:");
+    		userInput = reader.nextLine();
+    		inputFile = new File(userInput);
+    		flag = inputFile.exists() && inputFile.isFile();
+    	}
+    	Scanner fileReader = null;
+    	try {
+			fileReader = new Scanner(inputFile);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
 
         reader.close();
         
